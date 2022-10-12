eadf02a9905366bdcaf1c1008d740fe524b43d58
rodica <adigbononrodicaa@gmail.com>
2022-10-12 14:43:40 +0200

    splash-suscribe-login

---
+++ lib/Screen/Signup.dart
@@ -0,0 +1,445 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+import 'package:email_validator/email_validator.dart';
+import 'package:flutter_pw_validator/flutter_pw_validator.dart';
+
+import '../Screen/Login.dart';
+import '../Constant/Constant.dart';
+import '../Controller/UiController.dart';
+import 'package:avatar_glow/avatar_glow.dart';
+
+class SignUpView extends StatefulWidget {
+  const SignUpView({Key? key}) : super(key: key);
+
+  @override
+  State<SignUpView> createState() => _SignUpViewState();
+}
+
+class _SignUpViewState extends State<SignUpView> {
+  TextEditingController firstnameController = TextEditingController();
+  TextEditingController lastnameController = TextEditingController();
+  TextEditingController usernameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController passwordConfirmController = TextEditingController();
+
+  var password = "";
+
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    firstnameController.dispose();
+    lastnameController.dispose();
+    usernameController.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+    passwordConfirmController.dispose();
+    super.dispose();
+  }
+
+  SimpleUIController simpleUIController = Get.put(SimpleUIController());
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    var theme = Theme.of(context);
+
+    return GestureDetector(
+      onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        resizeToAvoidBottomInset: false,
+        body: SingleChildScrollView(
+          child: LayoutBuilder(
+            builder: (context, constraints) {
+              if (constraints.maxWidth > 600) {
+                return _buildLargeScreen(size, simpleUIController, theme);
+              } else {
+                return _buildSmallScreen(size, simpleUIController, theme);
+              }
+            },
+          ),
+        ),
+      ),
+    );
+  }
+
+  /// For large screens
+  Widget _buildLargeScreen(
+      Size size, SimpleUIController simpleUIController, ThemeData theme) {
+    return Row(
+      children: [
+        SizedBox(width: size.width * 0.06),
+        Expanded(
+          flex: 5,
+          child: _buildMainBody(size, simpleUIController, theme),
+        ),
+      ],
+    );
+  }
+
+  /// For Small screens
+  Widget _buildSmallScreen(
+      Size size, SimpleUIController simpleUIController, ThemeData theme) {
+    return Center(
+      child: _buildMainBody(size, simpleUIController, theme),
+    );
+  }
+
+  /// Main Body
+  Widget _buildMainBody(
+      Size size, SimpleUIController simpleUIController, ThemeData theme) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment:
+          size.width > 600 ? MainAxisAlignment.center : MainAxisAlignment.start,
+      children: [
+        SizedBox(
+          height: 20,
+          child: const DecoratedBox(
+            decoration: const BoxDecoration(color: Colors.white),
+          ),
+        ),
+        size.width > 600
+            ? Container()
+            : Center(
+                child: AvatarGlow(
+                  endRadius: 90,
+                  duration: Duration(seconds: 2),
+                  glowColor: Colors.white24,
+                  repeat: true,
+                  repeatPauseDuration: Duration(seconds: 2),
+                  startDelay: Duration(seconds: 1),
+                  child: Material(
+                    elevation: 8.0,
+                    shape: CircleBorder(),
+                    child: CircleAvatar(
+                      backgroundColor: Colors.transparent,
+                      backgroundImage:
+                          AssetImage('assets/images/logo_small.png'),
+                      radius: 50.0,
+                    ),
+                  ),
+                ),
+              ),
+        SizedBox(
+          height: size.height * 0.03,
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 20.0),
+          child: Text(
+            'Sign Up',
+            style: kLoginTitleStyle(size),
+          ),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 20.0, right: 20),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                // first name
+                TextFormField(
+                  style: kTextFormFieldStyle(),
+                  decoration: const InputDecoration(
+                    prefixIcon: Icon(Icons.person),
+                    hintText: 'First Name',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(15)),
+                    ),
+                  ),
+
+                  controller: firstnameController,
+                  // The validator receives the text that the user has entered.
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter first name';
+                    } else if (value.length < 4) {
+                      return 'at least enter 4 characters';
+                    } else if (value.length > 240) {
+                      return 'maximum character is 240';
+                    }
+                    return null;
+                  },
+                ),
+
+                SizedBox(
+                  height: size.height * 0.02,
+                ),
+
+                // last name
+                TextFormField(
+                  style: kTextFormFieldStyle(),
+                  decoration: const InputDecoration(
+                    prefixIcon: Icon(Icons.person),
+                    hintText: 'Last Name',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(15)),
+                    ),
+                  ),
+
+                  controller: lastnameController,
+                  // The validator receives the text that the user has entered.
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter last name';
+                    } else if (value.length < 4) {
+                      return 'at least enter 4 characters';
+                    } else if (value.length > 240) {
+                      return 'maximum character is 240';
+                    }
+                    return null;
+                  },
+                ),
+
+                SizedBox(
+                  height: size.height * 0.02,
+                ),
+
+                // username
+                TextFormField(
+                  style: kTextFormFieldStyle(),
+                  decoration: const InputDecoration(
+                    prefixIcon: Icon(Icons.person),
+                    hintText: 'Username',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(15)),
+                    ),
+                  ),
+
+                  controller: usernameController,
+                  // The validator receives the text that the user has entered.
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter username';
+                    } else if (value.length < 4) {
+                      return 'at least enter 4 characters';
+                    } else if (value.length > 13) {
+                      return 'maximum character is 13';
+                    }
+                    return null;
+                  },
+                ),
+
+                SizedBox(
+                  height: size.height * 0.02,
+                ),
+
+                // Email
+                TextFormField(
+                  style: kTextFormFieldStyle(),
+                  controller: emailController,
+                  decoration: const InputDecoration(
+                    prefixIcon: Icon(Icons.email_rounded),
+                    hintText: 'Email address',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(15)),
+                    ),
+                  ),
+                  // The validator receives the text that the user has entered.
+                  validator: (value) {
+                    final bool isValid = EmailValidator.validate(value!);
+
+                    if (isValid == 'no' || value.isEmpty) {
+                      return 'Please enter a valid email address';
+                    }
+
+                    return null;
+                  },
+                ),
+
+                SizedBox(
+                  height: size.height * 0.02,
+                ),
+
+                Column(
+                  children: [
+                    Obx(() => TextFormField(
+                          style: kTextFormFieldStyle(),
+                          controller: passwordController,
+                          obscureText: simpleUIController.isObscure.value,
+                          decoration: InputDecoration(
+                            prefixIcon: const Icon(Icons.lock_open),
+                            suffixIcon: IconButton(
+                              icon: Icon(
+                                simpleUIController.isObscure.value
+                                    ? Icons.visibility
+                                    : Icons.visibility_off,
+                              ),
+                              onPressed: () {
+                                simpleUIController.isObscureActive();
+                              },
+                            ),
+                            hintText: 'Password',
+                            border: const OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(15)),
+                            ),
+                          ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter a valid password';
+                            }
+                            return null;
+                          },
+                        )),
+                    SizedBox(
+                      height: size.height * 0.02,
+                    ),
+                    new FlutterPwValidator(
+                      controller: passwordController,
+                      minLength: 8,
+                      uppercaseCharCount: 1,
+                      numericCharCount: 3,
+                      specialCharCount: 1,
+                      normalCharCount: 3,
+                      width: 400,
+                      height: 150,
+                      onSuccess: (value) {
+                        password = value;
+                      },
+                      onFail: () {
+                        print("NOT MATCHED");
+                      },
+                    ),
+                  ],
+                ),
+
+                SizedBox(
+                  height: size.height * 0.03,
+                ),
+
+                // confirm password
+                Obx(
+                  () => TextFormField(
+                    style: kTextFormFieldStyle(),
+                    controller: passwordConfirmController,
+                    obscureText: simpleUIController.isObscure.value,
+                    decoration: InputDecoration(
+                      prefixIcon: const Icon(Icons.lock_open),
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          simpleUIController.isObscure.value
+                              ? Icons.visibility
+                              : Icons.visibility_off,
+                        ),
+                        onPressed: () {
+                          simpleUIController.isObscureActive();
+                        },
+                      ),
+                      hintText: 'Confirm password',
+                      border: const OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(15)),
+                      ),
+                    ),
+                    // The validator receives the text that the user has entered.
+                    validator: (value) {
+                      if (value != null &&
+                          value != password &&
+                          !value.isNotEmpty) {
+                        return 'The passwords are not the same';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+
+                SizedBox(
+                  height: size.height * 0.01,
+                ),
+
+                CheckboxListTile(
+                  title: Text(
+                      "Creating an account means you\'re okay with our Terms of Services and our Privacy Policy"),
+                  value: false,
+                  onChanged: (newValue) {},
+                  controlAffinity:
+                      ListTileControlAffinity.leading, //  <-- leading Checkbox
+                ),
+
+                /*Text(
+                  'Creating an account means you\'re okay with our Terms of Services and our Privacy Policy',
+                  style: kLoginTermsAndPrivacyStyle(size),
+                  textAlign: TextAlign.center,
+                ),*/
+
+                SizedBox(
+                  height: size.height * 0.02,
+                ),
+
+                /// SignUp Button
+                signUpButton(theme),
+                SizedBox(
+                  height: size.height * 0.03,
+                ),
+
+                /// Navigate To Login Screen
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(context,
+                        CupertinoPageRoute(builder: (ctx) => const Login()));
+                    firstnameController.clear();
+                    lastnameController.clear();
+                    usernameController.clear();
+                    emailController.clear();
+                    passwordController.clear();
+                    passwordConfirmController.clear();
+                    _formKey.currentState?.reset();
+
+                    simpleUIController.isObscure.value = true;
+                  },
+                  child: RichText(
+                    text: TextSpan(
+                      text: 'Already have an account?',
+                      style: kHaveAnAccountStyle(size),
+                      children: [
+                        TextSpan(
+                            text: " Login",
+                            style: kLoginOrSignUpTextStyle(size)),
+                      ],
+                    ),
+                  ),
+                ),
+
+                SizedBox(
+                  height: 5,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  // SignUp Button
+  Widget signUpButton(ThemeData theme) {
+    return SizedBox(
+      width: double.infinity,
+      height: 55,
+      child: ElevatedButton(
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(Colors.deepPurpleAccent),
+          shape: MaterialStateProperty.all(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15),
+            ),
+          ),
+        ),
+        onPressed: () {
+          // Validate returns true if the form is valid, or false otherwise.
+          if (_formKey.currentState!.validate()) {
+            // ... Navigate To your Home Page
+          }
+        },
+        child: const Text('Sign up'),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,256 +1,65 @@
-import 'package:english_words/english_words.dart';
+/*import 'Screen/Login.dart';
+import 'Constant/Constant.dart';
+import 'Screen/AnimatedSplashScreen.dart';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Startup Name Generator',
-      theme: ThemeData(
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Colors.white,
-          foregroundColor: Colors.pink,
-        ),
-      ),
-      home: const RandomWords(),
-    );
-  }
-}
-
-class _RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _saved = <WordPair>{};
-  final _biggerFont = const TextStyle(fontSize: 18);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.pink,
-        title: const Text('Dios délices'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.list),
-            onPressed: _pushSaved,
-            tooltip: 'Saved Suggestions',
-          ),
-        ],
-      ),
-      body: ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemBuilder: (context, i) {
-          if (i.isOdd) return const Divider();
-
-          final index = i ~/ 2;
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10));
-          }
-
-          final alreadySaved = _saved.contains(_suggestions[index]);
-
-          return ListTile(
-            title: Text(
-              _suggestions[index].asPascalCase,
-              style: _biggerFont,
-            ),
-            trailing: Icon(
-              alreadySaved ? Icons.favorite : Icons.favorite_border,
-              color: alreadySaved ? Colors.red : null,
-              semanticLabel: alreadySaved ? 'Remove from saved' : 'Save',
-            ),
-            onTap: () {
-              setState(() {
-                if (alreadySaved) {
-                  _saved.remove(_suggestions[index]);
-                } else {
-                  _saved.add(_suggestions[index]);
-                }
-              });
-            },
-          );
-        },
-      ),
-    );
-  }
-
-  void _pushSaved() {
-    Navigator.of(context).push(
-      MaterialPageRoute<void>(
-        builder: (context) {
-          final tiles = _saved.map(
-            (pair) {
-              return ListTile(
-                title: Text(
-                  pair.asPascalCase,
-                  style: _biggerFont,
-                ),
-              );
-            },
-          );
-          final divided = tiles.isNotEmpty
-              ? ListTile.divideTiles(
-                  context: context,
-                  tiles: tiles,
-                ).toList()
-              : <Widget>[];
-
-          return Scaffold(
-            appBar: AppBar(
-              title: const Text('Saved Suggestions'),
-            ),
-            body: ListView(children: divided),
-          );
-        },
-      ),
-    );
-  }
-}
-
-class RandomWords extends StatefulWidget {
-  const RandomWords({super.key});
-
-  @override
-  State<RandomWords> createState() => _RandomWordsState();
-}
-/*class MainActivity extends StatelessWidget {
-  const MainActivity({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-
-    return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.pink,
-        title: const Text('Dios délices'),
-      ),
-      body: Center(
-        child: Text(wordPair.asPascalCase),
-      ),
-    ));
-  }
+Future main() async {
+  SystemChrome.setEnabledSystemUIOverlays([]);
+  runApp(new MaterialApp(
+    title: 'dios_delices',
+    debugShowCheckedModeBanner: false,
+    theme: new ThemeData(
+      primarySwatch: Colors.red,
+    ),
+    home: new AnimatedSplashScreen(),
+    routes: <String, WidgetBuilder>{
+      ANIMATED_SPALSH: (BuildContext context) => new AnimatedSplashScreen(),
+      // LOGIN_SCREEN: (BuildContext context) => new LoginPage(),
+      LOGIN: (BuildContext context) => new Login()
+    },
+  ));
 }*/
 
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'Screen/Signup.dart';
+import 'Screen/Login.dart';
+import 'Constant/Constant.dart';
+import 'Screen/AnimatedSplashScreen.dart';
 
-/*import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() {
+Future main() async {
+  SystemChrome.setEnabledSystemUIOverlays([]);
+  runApp(new MaterialApp(
+    title: 'dios_delices',
+    debugShowCheckedModeBanner: false,
+    theme: new ThemeData(
+      primarySwatch: Colors.red,
+    ),
+    home: new AnimatedSplashScreen(),
+    routes: <String, WidgetBuilder>{
+      ANIMATED_SPALSH: (BuildContext context) => new AnimatedSplashScreen(),
+      LOGIN_SCREEN: (BuildContext context) => new SignUpView(),
+      LOGIN: (BuildContext context) => new Login()
+    },
+  ));
+}
+
+/*void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.deepPurple,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    return const GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SignUpView(),
     );
   }
 }*/
